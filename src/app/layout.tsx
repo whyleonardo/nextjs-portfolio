7cb074cc104a375b243a3029989f27685606ecb5
whyleonardo <pleasefloyd73@gmail.com>
2023-02-07 16:52:35 -0300

    chore: initial config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,23 @@
-import './globals.css'
+import '@/app/styles/globals.css'
+import { Inter } from '@next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode
+	children: React.ReactNode
 }) {
-  return (
-    <html lang="en">
-      {/*
+	return (
+		<html>
+			{/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-      <head />
-      <body>{children}</body>
-    </html>
-  )
+			<head />
+			<body className={`bg-slate-900 text-white ${inter.className}`}>
+				{children}
+			</body>
+		</html>
+	)
 }
